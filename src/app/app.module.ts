96ffe1b5a1ff840b1a2577716dc0cc3a7a58845a
CannyRo <whatcannydev@gmail.com>
2023-05-20 20:34:28 +0200

    rework of home with component approach

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './componentes/home/home.component';
 import { NosotrosComponent } from './componentes/nosotros/nosotros.component';
+import { MenuComponent } from './componentes/menu/menu.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { MainComponent } from './componentes/main/main.component';
+import { CarouselComponent } from './componentes/carousel/carousel.component';
+import { ArgumentsComponent } from './componentes/arguments/arguments.component';
+import { InfoComponent } from './componentes/info/info.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NosotrosComponent
+    NosotrosComponent,
+    MenuComponent,
+    FooterComponent,
+    MainComponent,
+    CarouselComponent,
+    ArgumentsComponent,
+    InfoComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/componentes/carousel/carousel.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carousel',
+  templateUrl: './carousel.component.html',
+  styleUrls: ['./carousel.component.scss']
+})
+export class CarouselComponent {
+  imagen1:string="../../../assets/images/imagen1.png"
+  imagen2:string="../../../assets/images/imagen2.png"
+}
