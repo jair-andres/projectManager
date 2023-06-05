6435d4777d26d0248042f3a8a1a1d29fd5db5c7e
CannyRo <whatcannydev@gmail.com>
2023-06-05 05:11:26 +0200

    insignia con el nombre del creador del proyecto por defecto

--- src/app/componentes/create-project/create-project.component.ts
+++ src/app/componentes/create-project/create-project.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { MensajesService } from 'src/app/servicios/mensajes.service';
 import { PeticionUsuariosService } from 'src/app/servicios/peticion-usuarios.service';
@@ -8,9 +8,14 @@ import { PeticionUsuariosService } from 'src/app/servicios/peticion-usuarios.ser
   templateUrl: './create-project.component.html',
   styleUrls: ['./create-project.component.scss']
 })
-export class CreateProjectComponent {
+export class CreateProjectComponent implements OnInit{
+
   constructor(private peticion:PeticionUsuariosService, private msg:MensajesService, private route:Router){}
 
+  id:string = ""
+  nombre:string = ""
+  email:string = ""
+  //
   nombreProyecto:string = ""
   descripcionProyecto:string = ""
   objetivoProyecto:string = ""
@@ -22,7 +27,30 @@ export class CreateProjectComponent {
   busqueda:string =""
   resultadoDeBusqueda:string[] = []
 
+  ngOnInit(){
+    this.miData()
+  }
 
+  miData(){
+    let post = {
+      hots:this.peticion.urllocal,
+      path:"miData",
+      payload:{}
+    }
+    this.peticion.Post(post.hots + post.path,post.payload).then((res:any) => {
+      console.log("RES :",res)
+      this.id = res.id
+      this.nombre = res.nombre
+      this.email = res.email
+      let yo = { 
+        id:this.id,
+        nombre:this.nombre,
+        email:this.email
+      }
+      this.miembros.push(yo)
+    })
+    
+  }
 
   GuardarProyecto(){
     let post = {
@@ -52,7 +80,6 @@ export class CreateProjectComponent {
       }
     })
   }
-
 
   ActualizarMiembros(newArrayMembers:string[]){
     this.miembros.push(newArrayMembers[0])
