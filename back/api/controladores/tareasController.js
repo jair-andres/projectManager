99bb2ccc9fe9baf2171e312ba1d5c17358fce3aa
CannyRo <whatcannydev@gmail.com>
2023-06-13 09:50:56 +0200

    actualizar mis tareas porque soy encargado y eliminar mis tareas solo si soy el creador del proyecto

--- back/api/controladores/tareasController.js
+++ back/api/controladores/tareasController.js
@@ -176,82 +176,96 @@ tareasController.Actualizar = function (request, response) {
     post.id == null ||
     post.id.trim() == ""
   ) {
-    response.json({ state: false, mensaje: "El campo nombre es obligatorio" });
+    response.json({ state: false, mensaje: "El campo id es obligatorio" });
     return false;
   }
 
-  if (
-    post.titulo == undefined ||
-    post.titulo == null ||
-    post.titulo.trim() == ""
-  ) {
-    response.json({ state: false, mensaje: "El campo nombre es obligatorio" });
-    return false;
+  if(post.titulo){
+    if (
+      post.titulo == undefined ||
+      post.titulo == null ||
+      post.titulo.trim() == ""
+    ) {
+      response.json({ state: false, mensaje: "El campo titulo es obligatorio" });
+      return false;
+    }
   }
-
-  // if (
-  //   post.miembros == undefined ||
-  //   post.miembros == null ||
-  //   post.miembros.trim() == ""
-  // ) {
-  //   response.json({ state: false, mensaje: "El campo nombre es obligatorio" });
-  //   return false;
-  // }
-
-  if (
-    post.descripcion == undefined ||
-    post.descripcion == null ||
-    post.descripcion.trim() == ""
-  ) {
-    response.json({ state: false, mensaje: "El campo nombre es obligatorio" });
-    return false;
+  
+  if(post.descripcion){
+    if (
+      post.descripcion == undefined ||
+      post.descripcion == null ||
+      post.descripcion.trim() == ""
+    ) {
+      response.json({ state: false, mensaje: "El campo descripcion es obligatorio" });
+      return false;
+    }
   }
-
-  if (
-    post.fechaInicio == undefined ||
-    post.fechaInicio == null ||
-    post.fechaInicio.trim() == ""
-  ) {
-    response.json({ state: false, mensaje: "El campo nombre es obligatorio" });
-    return false;
+  
+  if(post.fechaInicio){
+    if (
+      post.fechaInicio == undefined ||
+      post.fechaInicio == null ||
+      post.fechaInicio.trim() == ""
+    ) {
+      response.json({ state: false, mensaje: "El campo fecha de inicio es obligatorio" });
+      return false;
+    }
   }
-
-  if (
-    post.fechaFinal == undefined ||
-    post.fechaFinal == null ||
-    post.fechaFinal.trim() == ""
-  ) {
-    response.json({ state: false, mensaje: "El campo nombre es obligatorio" });
-    return false;
+  
+  if(post.fechaFinal){
+    if (
+      post.fechaFinal == undefined ||
+      post.fechaFinal == null ||
+      post.fechaFinal.trim() == ""
+    ) {
+      response.json({ state: false, mensaje: "El campo fecha final es obligatorio" });
+      return false;
+    }
   }
-
-  if (
-    post.estado == undefined ||
-    post.estado == null ||
-    post.estado.trim() == ""
-  ) {
-    response.json({ state: false, mensaje: "El campo nombre es obligatorio" });
-    return false;
+  
+  if(post.estado){
+    if (
+      post.estado == undefined ||
+      post.estado == null ||
+      post.estado.trim() == ""
+    ) {
+      response.json({ state: false, mensaje: "El campo estado es obligatorio" });
+      return false;
+    }
   }
-
-  if (
-    post.actividades == undefined ||
-    post.actividades == null ||
-    post.actividades.trim() == ""
-  ) {
-    response.json({ state: false, mensaje: "El campo nombre es obligatorio" });
-    return false;
+  
+  if(post.actividades){
+    if (
+      post.actividades == undefined ||
+      post.actividades == null //||
+      //post.actividades == []
+    ) {
+      response.json({ state: false, mensaje: "El campo actividades es obligatorio" });
+      return false;
+    }
   }
-
-  if (
-    post.comentarios == undefined ||
-    post.comentarios == null ||
-    post.comentarios.trim() == ""
-  ) {
-    response.json({ state: false, mensaje: "El campo nombre es obligatorio" });
-    return false;
+  
+  if(post.comentarios){
+    if (
+      post.comentarios == undefined ||
+      post.comentarios == null //||
+      // post.comentarios == []
+    ) {
+      response.json({ state: false, mensaje: "El campo comentarios es obligatorio" });
+      return false;
+    }
   }
-
+  tareasModel.Actualizar(post, function (respuesta) {
+    if (respuesta.state == false) {
+      response.json({
+        state: false,
+        mensaje: "se presento un error al actualizar",
+      });
+    } else {
+      response.json({ state: true, mensaje: "se actualizo correctamente" });
+    }
+  });
 }
 
 tareasController.Eliminar = function (request, response) {
--- src/app/componentes/tasks/tasks.component.ts
+++ src/app/componentes/tasks/tasks.component.ts
@@ -11,7 +11,15 @@ declare var window: any;
 export class TasksComponent implements OnInit {
 
   leerTareaModal:any
+  eliminarTareaModal:any
   tareas:any[] =[]
+
+  miId:string =""
+  miAlias:string =""
+  misTareasUsuario:any[] = []
+  tareasDelProyecto:any[] = []
+
+  idCreador:string =""
 
   idTarea:string =""
   unaTarea:any[] = []
@@ -35,6 +43,9 @@ export class TasksComponent implements OnInit {
     this.leerTareaModal = new window.bootstrap.Modal(
       document.getElementById('leerTareaModal')
     );
+    this.eliminarTareaModal = new window.bootstrap.Modal(
+      document.getElementById('eliminarTareaModal')
+    );
     this.miData()
   }
   miData(){
@@ -44,8 +55,28 @@ export class TasksComponent implements OnInit {
       payload:{}
     }
     this.peticion.Post(post.hots + post.path,post.payload).then((res:any) => {
+      this.miId = res.id
+      this.miAlias = res.alias
       this.CargarTodasMisTareas(res.id)
     })
+  }
+  QuienEsElCreadorDelProyecto(idProyecto:string){
+    let post = {
+      host:this.peticion.urllocal,
+      path:"Proyectos/CargarId",
+      payload:{
+        id:idProyecto
+      }
+    }
+    this.peticion.Post(post.host + post.path, post.payload).then((res:any) => {
+      if(res.state == false){
+        this.msg.Load(res.mensaje, "danger", 5000)
+      } else {
+        this.idCreador = res?.datos[0].keyUser
+        this.tareasDelProyecto = res?.datos[0].tareas
+      }
+    })
+
   }
   CargarTodasMisTareas(idUser:string) {
     let post = {
@@ -57,7 +88,10 @@ export class TasksComponent implements OnInit {
     }
     this.peticion.Post(post.host + post.path, post.payload).then((res:any) => {
       this.tareas=res?.datos[0].misTareasinfo
+      this.misTareasUsuario=res?.datos[0].misTareas
       console.log("Mis tareas :",this.tareas);
+      console.log("DATAS => ",res?.datos[0])
+      console.log("misTareas => ",this.misTareasUsuario)
     })
   }
   CortarPalabra(palabra:string, tamano:number){
@@ -72,7 +106,7 @@ export class TasksComponent implements OnInit {
       return palabra
     }
   }
-  EditarUnaTarea(idTarea:string){
+  EditarUnaTarea(idTarea:string,conModal:boolean){
     let post = {
       host:this.peticion.urllocal,
       path:"Tareas/CargarId",
@@ -83,34 +117,154 @@ export class TasksComponent implements OnInit {
     this.peticion.Post(post.host + post.path, post.payload).then((res:any) => {
       this.unaTarea=res?.datos[0]
       console.log("Una tarea :",this.unaTarea);
+      this.idTarea = res?.datos[0]._id
       this.titulo = res?.datos[0].titulo
       this.descripcion = res?.datos[0].descripcion
       this.fechaInicio = res?.datos[0].fechaInicio
       this.fechaFinal = res?.datos[0].fechaFinal
       this.keyProyecto = res?.datos[0].keyProyecto
       this.keyEncargado = res?.datos[0].keyEncargado
+      this.actividades = res?.datos[0].actividades
       this.comentarios = res?.datos[0].comentarios
       this.estado = res?.datos[0].estado
-      this.leerTareaModal.show();
+      this.QuienEsElCreadorDelProyecto(this.keyProyecto)
+      if(conModal == true){
+        this.leerTareaModal.show();
+      }
+    })
+  }
+  ActualizarTarea(){
+    if(this.nuevoComentario !== undefined && this.nuevoComentario !== null && this.nuevoComentario !== ""){
+      let today = new Date();
+      let creoUnNuevoComentario:any = {
+        date : today,
+        autor : this.miAlias,
+        autorId : this.miId,
+        comentario : this.nuevoComentario
+      }
+      if(this.comentarios == undefined){
+        this.comentarios = []
+        this.comentarios.push(creoUnNuevoComentario)
+      } else {
+        this.comentarios.push(creoUnNuevoComentario)
+      }
+    }
+    
+    let post = {
+      hots:this.peticion.urllocal,
+      path:"Tareas/Actualizar",
+      payload:{
+        id:this.idTarea,
+        descripcion:this.descripcion,
+        fechaInicio:this.fechaInicio,
+        fechaFinal:this.fechaFinal,
+        actividades:this.actividades,
+        comentarios:this.comentarios,
+        estado:this.estado,
+      }
+    }
+    this.peticion.Post(post.hots + post.path,post.payload).then((res:any) => {
+      if(res.state == false){
+        this.msg.Load(res.mensaje, "danger", 5000)
+      } else {
+        this.msg.Load(res.mensaje, "success", 5000)
+        this.leerTareaModal.toggle();
+        location.reload()
+      }
+    })
+
+  }
+  QuieroEliminarEstaTarea(idTarea:string, tituloTarea:string){
+    console.log("Eliminar esta tarea :",idTarea,tituloTarea)
+    this.EditarUnaTarea(idTarea,false)
+    let foo = idTarea
+    this.eliminarTareaModal.show(idTarea)
+  }
+
+  EliminarEstaTarea(idTarea:string){
+    console.log("Si!!! Elimino esta tarea!!! => ",idTarea)
+    let post = {
+      hots:this.peticion.urllocal,
+      path:"Tareas/Eliminar",
+      payload:{
+        id:idTarea
+      }
+    }
+    this.peticion.Post(post.hots + post.path,post.payload).then((res:any) => {
+      if(res.state == false){
+        this.msg.Load(res.mensaje, "danger", 5000)
+      } else {
+        this.msg.Load(res.mensaje, "success", 5000)
+        this.eliminarTareaModal.toggle();
+        // Quitar la tarea en misTareas del Usuario
+        let indexParaMisTareasUsuarios:number = this.misTareasUsuario.indexOf(idTarea)
+        console.log("INDEX 1 => ",indexParaMisTareasUsuarios)
+        this.misTareasUsuario.splice(indexParaMisTareasUsuarios,1)
+        console.log("Nouveau this.misTareasUsuario : ",this.misTareasUsuario)
+        // Quitar la tarea en tareas de proyecto
+        let indexParaTareasDelProyecto:number = this.tareasDelProyecto.indexOf(idTarea)
+        console.log("INDEX 2 => ",indexParaTareasDelProyecto)
+        this.tareasDelProyecto.splice(indexParaTareasDelProyecto,1)
+        console.log("Nouveau this.tareasDelProyecto : ",this.misTareasUsuario)
+        // Actualizacion en Usuarios y Proyectos
+        this.ActualizarMisTareasEnUsuario(this.miId)
+        this.ActualizarTareasEnProyecto(this.keyProyecto)
+      }
+    })
+    // Eliminar de la base Tarea
+    // Actuaizar misTareas en la base Usuario
+    // Actualizar tareas en la base Proyecto
+  }
+
+  // QuitarMiembros(idMiembro:any, miembros:any[]){
+  //   let foe:any = (elArray:any) => elArray.id == idMiembro
+  //   let quitarEsteIndex:number = miembros.findIndex(foe)
+  //   let suNombre:string = this.miembros[quitarEsteIndex]?.alias
+  //   let quitarLo:any = this.miembros.splice(quitarEsteIndex,1)
+  //   let mensaje:string = `${suNombre} eliminado con éxito`
+  //   this.msg.Load(mensaje, "success", 5000)
+  // }
+
+  ActualizarMisTareasEnUsuario(idUsuario:string){
+    let post = {
+      hots:this.peticion.urllocal,
+      path:"Usuarios/Actualizar",
+      payload:{
+        id:idUsuario,
+        misTareas:this.misTareasUsuario
+      }
+    }
+    this.peticion.Post(post.hots + post.path,post.payload).then((res:any) => {
+      if(res.state == false){
+        this.msg.Load(res.mensaje, "danger", 5000)
+      } else {
+        this.msg.Load(res.mensaje, "success", 5000)
+      }
     })
   }
 
-  ActualizarTarea(){
-    console.log("this.tareas : ",this.tareas)
+  ActualizarTareasEnProyecto(idProyecto:string){
+    let post = {
+      hots:this.peticion.urllocal,
+      path:"Proyectos/Actualizar",
+      payload:{
+        id:idProyecto,
+        tareas:this.tareasDelProyecto
+      }
+    }
+    this.peticion.Post(post.hots + post.path,post.payload).then((res:any) => {
+      if(res.state == false){
+        this.msg.Load(res.mensaje, "danger", 5000)
+      } else {
+        this.msg.Load(res.mensaje, "success", 5000)
+      }
+    })
+  }
 
-    console.log("this.idTarea : ",this.idTarea)
-    console.log("this.unaTarea : ",this.unaTarea)
-
-    console.log("this.titulo : ",this.titulo)
-    console.log("this.descripcion : ",this.descripcion)
-    console.log("this.fechaInicio : ",this.fechaInicio)
-    console.log("this.fechaFinal : ",this.fechaFinal)
-    console.log("this.keyProyecto : ",this.keyProyecto)
-    console.log("this.keyEncargado : ",this.keyEncargado)
-    console.log("this.actividades : ",this.actividades)
-    console.log("this.comentarios : ",this.comentarios)
-    console.log("this.estado : ",this.estado)
-
-    console.log("this.nuevoComentario : ",this.nuevoComentario)
+  consoleActividades(){
+    console.log(this.actividades)
+  }
+  consoleTareas(){
+    console.log(this.tareas)
   }
 }
